7b35238f7f880850ec0ded25d42b9899e4233b80
kukuhsetiawan13 <study@kukuh.anonaddy.me>
2022-12-28 21:01:46 +0700

    Initial setup and tailwind config

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
