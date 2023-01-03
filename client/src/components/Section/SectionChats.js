044d71d92ce949e5b208904821ba496673794a3d
ersakantibelva <ersakantibelva@gmail.com>
2023-01-03 23:01:16 +0700

    add responsiveness in chats section

--- client/src/components/Section/SectionChats.js
+++ client/src/components/Section/SectionChats.js
@@ -5,147 +5,147 @@ import avatar from '../../pictures/avatar-1.3921191a8acf79d3e907.jpg'
 export default function SectionChats() {
   return (
     <>
-        <div className='text-white text-xl mb-8'>
+        <div className='mb-8 text-xl text-white'>
           Chats
         </div>
 
-        <div className='bg-gray search-chat-container rounded flex items-center'>
-          <div className='w-1/6 flex justify-center'>
+        <div className='flex items-center w-full rounded h-14 bg-light-gray lg:h-18'>
+          <div className='flex items-center justify-center w-1/6'>
             <FontAwesomeIcon className='text-gray-400' icon='magnifying-glass'/>
           </div>
-          <div className='w-5/6  flex justify-center'>
-            <input className='bg-transparent text-white focus:border-none focus:outline-none' type='text' placeholder='Search messages or users'></input>
+          <div className='flex w-5/6'>
+            <input className='pr-4 text-white bg-transparent focus:border-none focus:outline-none' type='text' placeholder='Search messages or users'></input>
           </div>
         </div>
 
-        <div className='flex flex-col my-10 gap-3 max-h-160 overflow-y-auto'>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+        <div className='flex flex-col h-full gap-3 mt-5 overflow-y-auto'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
-          <div className='flex items-center hover:bg-gray-700 cursor-pointer rounded p-2 gap-4'>
+          <div className='flex items-center gap-4 p-2 rounded cursor-pointer hover:bg-gray-700'>
             <img src={avatar} class='avatar-chat' alt='avatar'></img>
             <div className='flex flex-col w-full gap-1'>
               <div className='flex items-center justify-between'>
-                <h4 className='text-white text-base'>Patrick Hendriks</h4>
-                <h5 className='text-gray-300 text-sm'>02:50</h5>
+                <h4 className='text-base text-white'>Patrick Hendriks</h4>
+                <h5 className='text-sm text-gray-300'>02:50</h5>
               </div>
               <div className='flex items-center justify-between'>
-                <h4 className='text-gray-400 text-sm'>okay sure</h4>
-                <div className='bg-red-900-blur font-bold text-center rounded-full w-5 h-5 text-red-700 text-sm'>2</div>
+                <h4 className='text-sm text-gray-400'>okay sure</h4>
+                <div className='w-5 h-5 text-sm font-bold text-center text-red-700 rounded-full bg-red-900-blur'>2</div>
               </div>
             </div>
           </div>
