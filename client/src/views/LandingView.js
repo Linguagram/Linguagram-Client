ae657b376c98bb0f635a2cd1c5aac93a41a64750
ekadk <ekadharmakusuma3@gmail.com>
2023-01-05 20:08:21 +0800

    change: bg-color; fix: header overlay size

--- client/src/views/LandingView.js
+++ client/src/views/LandingView.js
@@ -1,6 +1,6 @@
 export default function LandingView() {
   return (
-    <div className="flex flex-col flex-1 min-h-screen bg-black text-white">
+    <div className="flex flex-col flex-1 min-h-screen bg-darker-gray text-white">
       {/* Hero */}
       <header
         className="flex flex-col h-screen bg-cover bg-center justify-center md:justify-center items-center lg:h-screen md:bg-bottom"
@@ -8,16 +8,18 @@ export default function LandingView() {
           backgroundImage:
             "url(https://images.unsplash.com/photo-1473679408190-0693dd22fe6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)",
         }}>
-        <p className="pt-2 text-xl bg-black bg-opacity-50 w-full text-center lg:text-3xl md:p-0 lg:bg-opacity-0">
-          Learn any language from native.
-        </p>
-        <h1 className="text-3xl font-bold bg-black bg-opacity-50 w-full mx-8 px-2 pb-2 text-center mb-2 md:text-7xl md:mb-8 md:bg-opacity-0 md:p-0">
-          Speak your mind <br /> in any tounge.
-        </h1>
-        <div className="flex flex-col items-center lg:mb-36">
-          <button className="bg-main-color rounded py-2 px-8 ">
-            Get Started
-          </button>
+        <div className="bg-black h-screen flex flex-col w-full text-center items-center justify-center bg-opacity-40">
+          <p className="pt-2 text-xl  w-full lg:text-3xl md:p-0">
+            Learn any language from native.
+          </p>
+          <h1 className="text-3xl font-bold w-full mx-8 px-2 pb-2 mb-2 md:text-7xl md:mb-8 md:p-0">
+            Speak your mind <br /> in any tounge.
+          </h1>
+          <div className="flex flex-col items-center lg:mb-36">
+            <button className="bg-main-color rounded py-2 px-8 ">
+              Get Started
+            </button>
+          </div>
         </div>
       </header>
       <main className="container mx-auto min-h-screen lg:min-h-max py-4">
@@ -56,9 +58,16 @@ export default function LandingView() {
         </div>
 
         <div className="flex flex-col items-center px-8 justify-center flex-1 lg:max-w-screen-sm lg:mx-auto my-16">
-          <h1 className="text-center font-bold text-4xl py-2 mb-4">Stefanie and Anne connect on Linguagram...</h1>
-          <h3 className="text-lg lg:text-2xl font-light text-center mb-4">...and use Linguagram to practice their target language together! Stefania helps Anne with German and Anne helps Stefania with English</h3>
-          <h3 className="text-lg lg:text-2xl font-light text-center mb-8">Join Stefania and Anne and start improve your language!</h3>
+          <h1 className="text-center font-bold text-4xl py-2 mb-4">
+            Stefanie and Anne connect on Linguagram...
+          </h1>
+          <h3 className="text-lg lg:text-2xl font-light text-center mb-4">
+            ...and use Linguagram to practice their target language together!
+            Stefania helps Anne with German and Anne helps Stefania with English
+          </h3>
+          <h3 className="text-lg lg:text-2xl font-light text-center mb-8">
+            Join Stefania and Anne and start improve your language!
+          </h3>
           <button className="bg-main-color rounded py-2 px-8 ">
             Get Started
           </button>
