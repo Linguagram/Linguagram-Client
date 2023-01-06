4d8d48c239a3dcd1600885a393123c2ed9b130e3
ersakantibelva <ersakantibelva@gmail.com>
2023-01-06 23:54:15 +0700

    fix: bug fixed from merging

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ import {
   faCommentDots,
   faBars,
   faXmark,
-  faRightFromBracket
+  faRightFromBracket,
   faLock,
   faEnvelope,
   faPhone
@@ -66,7 +66,7 @@ library.add(
   faUserLarge,
   faBars,
   faXmark,
-  faRightFromBracket
+  faRightFromBracket,
   faLock,
   faEnvelope,
   faPhone
