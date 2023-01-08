05dd918ea62a6a6b73eb8e4074560a1b3f1dae76
ekadk <ekadharmakusuma3@gmail.com>
2023-01-09 01:17:52 +0800

    refactor: friend request card; fix: change h-3/4 to max-h-3/4 in request card container

---
+++ client/src/components/Cards/FriendRequestCard.js
@@ -0,0 +1,26 @@
+import avatar from "../../pictures/avatar-1.3921191a8acf79d3e907.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function FriendRequestCard() {
+  return (
+    <div>
+      <div className="flex items-center gap-4 p-2 rounded">
+        <img src={avatar} className="avatar-chat" alt="avatar"></img>
+        <div className="flex justify-between w-full gap-1">
+          <div className="flex items-center justify-between">
+            <h4 className="text-base text-white cursor-pointer hover:underline">
+              Patrick Hendriks
+            </h4>
+          </div>
+          <div className="flex items-center justify-between gap-6">
+            <h6 className="text-2xl text-gray-300 cursor-pointer ">&times;</h6>
+            <FontAwesomeIcon
+              className="text-base text-gray-300 cursor-pointer "
+              icon="check"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
