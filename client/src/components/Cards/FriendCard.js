08b2cc818a89810e76a029970e6a432017bee8fa
ekadk <ekadharmakusuma3@gmail.com>
2023-01-09 16:31:04 +0800

    fix: add items-center className to FriendCard content

--- client/src/components/Cards/FriendCard.js
+++ client/src/components/Cards/FriendCard.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function FriendCard({friend}) {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center">
       <h5 className="text-white cursor-pointer">{friend.Friend.username}</h5>
       <FontAwesomeIcon
         className="text-gray-400 cursor-pointer"
