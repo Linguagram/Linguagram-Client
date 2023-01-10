1e3650cb2ad9a113f4ff85dc3d17f29e56ea6ba0
freyrads <frs18840@gmail.com>
2023-01-10 09:28:23 +0700

    scroll only with new message

--- client/src/components/Chatroom/ChatRoomMessage.js
+++ client/src/components/Chatroom/ChatRoomMessage.js
@@ -1,19 +1,25 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import ChatBubble from './ChatBubble'
 
 export default function ChatRoomMessage() {
     const messageEndRef = useRef(null)
     const { currentMessages } = useSelector((state) => state.messageReducer);
-    const { thisUser, counterpartUser } = useSelector((state) => state.userReducer);
+    const [ currentMessagesLength, setCurrentMessagesLength ] = useState(0);
+    // const { thisUser, counterpartUser } = useSelector((state) => state.userReducer);
 
     const scrollToBottom = () => {
         messageEndRef.current?.scrollIntoView({ behavior: "smooth" })
     }
 
     useEffect(() => {
-        scrollToBottom()
+        if (currentMessagesLength !== currentMessages.length)
+            setCurrentMessagesLength(currentMessages.length);
     }, [currentMessages])
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [currentMessagesLength]);
 
   return (
     <div className='flex flex-col flex-grow p-5 overflow-x-hidden overflow-y-auto'>
