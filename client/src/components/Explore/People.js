db2f7341003103078f2d9d72dae4c66c2e74e40e
ekadk <ekadharmakusuma3@gmail.com>
2023-01-11 17:30:38 +0800

    fix: matching result message in explore/people

--- client/src/components/Explore/People.js
+++ client/src/components/Explore/People.js
@@ -50,7 +50,7 @@ export default function People() {
             )}
           </div>
         </div>
-        <div className="grid justify-center w-full h-full grid-cols-1 px-4 mx-auto mt-8 md:px-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 scrollbar-hide">
+        <div className="grid justify-center w-full grid-cols-1 px-4 mx-auto mt-8 md:px-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 scrollbar-hide">
           {users.map((user) => {
             return (
               <PeopleCard
