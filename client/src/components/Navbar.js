00798f6ab7cfcf73ee4857bf820a15825d91aa25
ekadk <ekadharmakusuma3@gmail.com>
2023-01-11 16:27:25 +0800

    fix: landing drawer button and animation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,10 +19,11 @@ export default function Navbar() {
   return (
     <>
       <div
-        className={`flex flex-1 justify-end ease-in-out duration-500  ${
-          drawer ? "translate-x-0" : "translate-x-full"
+        className={`flex flex-1 justify-end ${
+          drawer ? "translate-x-0" : "translate-x-full delay-500"
         }`}>
-        <div className="h-screen bg-darker-gray fixed w-3/5 z-100 p-4 flex flex-1 gap-4 flex-col">
+        <div className="h-screen w-screen fixed flex justify-end bg-black bg-opacity-40">
+        <div className={`w-3/5 bg-darker-gray h-full z-100 p-4 flex gap-4 flex-col ease-in-out duration-500 ${drawer ? "translate-x-0" : "translate-x-full"}`}>
           <div className="flex justify-end mb-2">
             <button onClick={toggleDrawer}>
               <FontAwesomeIcon icon="xmark" className="text-white text-2xl" />
@@ -31,6 +32,7 @@ export default function Navbar() {
           <div className="h-16 text-2xl text-white font-bold ">
             <NavLink
               to={"/"}
+              onClick={toggleDrawer}
               className="flex justify-center items-center w-full">
               <img
                 src="https://ik.imagekit.io/enybtlxa2/linguagram.-logo-only-cropped.png?ik-sdk-version=javascript-1.4.3&updatedAt=1673084814327"
@@ -42,12 +44,16 @@ export default function Navbar() {
           </div>
           <div className="flex flex-col text-xl text-white font-bold gap-2">
             <div className="border-b border-white-500 py-2">
-              <NavLink to={"/register"}>Register</NavLink>
+              <NavLink to={"/"} onClick={toggleDrawer}>Home</NavLink>
             </div>
             <div className="border-b border-white-500 py-2">
-              <NavLink to={"/login"}>Login</NavLink>
+              <NavLink to={"/register"} onClick={toggleDrawer}>Register</NavLink>
+            </div>
+            <div className="border-b border-white-500 py-2">
+              <NavLink to={"/login"} onClick={toggleDrawer}>Login</NavLink>
             </div>
           </div>{" "}
+        </div>
         </div>
       </div>
 
