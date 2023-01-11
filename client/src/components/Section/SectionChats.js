a7023874ee2c1f55b85bc063a9af880f54e7d892
freyrads <frs18840@gmail.com>
2023-01-11 18:52:59 +0700

    resolve conflict

--- client/src/components/Section/SectionChats.js
+++ client/src/components/Section/SectionChats.js
@@ -102,7 +102,6 @@ export default function SectionChats() {
 
                 <div className="flex flex-col w-full gap-1">
                   <div className="flex items-center justify-between">
-<<<<<<< Updated upstream
                     { group.type === "dm" ? (
                       group.GroupMembers[0] &&
                         group.GroupMembers[0].UserId === thisUser.id ? (
@@ -117,22 +116,6 @@ export default function SectionChats() {
                     ) : ( 
                         <h4 className="text-base text-white">{group.name}</h4>
                       )}
-=======
-                    {  group.name === "private" ? ( 
-                    group.GroupMembers[0] &&
-                      group.GroupMembers[0].UserId === thisUser.id ? (
-                        <h4 className="text-base text-white">
-                          {group.GroupMembers[1].User.username}
-                        </h4>
-                      ) : (
-                        <h4 className="text-base text-white">
-                          {group.GroupMembers[0].User.username}
-                        </h4>
-                      )
-                      ) : ( 
-                       <h4 className="text-base text-white">{group.name}</h4>
-                     )} 
->>>>>>> Stashed changes
                     <h5 className="text-sm text-gray-300">
                       {group.Messages.length > 0
                         ? `${group.Messages[0].createdAt.getHours()}.${group.Messages[0].createdAt.getMinutes()}`
