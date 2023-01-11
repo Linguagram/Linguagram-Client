9a66f12c925f4a35ebf3f36f459b66ec2732c748
freyrads <frs18840@gmail.com>
2023-01-11 20:15:38 +0700

    add background color for friend

--- client/src/components/Containers/FriendCardContainers.js
+++ client/src/components/Containers/FriendCardContainers.js
@@ -1,11 +1,12 @@
 import FriendCard from "../Cards/FriendCard";
+import "./FriendCard.css";
 
 export default function FriendCardContainers({ letter, friends }) {
   return (
     <div className="flex flex-col gap-3">
       <div className="flex flex-col">
         <div className="text-light-blue">{letter}</div>
-        <div className="flex flex-col gap-5 pt-2 pl-2">
+        <div className="flex flex-col gap-5 p-2 pl-2 friend-card-hover">
           {friends.map((friend) => {
             return <FriendCard key={friend.id} friend={friend} />;
           })}
